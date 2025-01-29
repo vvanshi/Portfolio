624cb15b7f00bec33741bbdf92a29cb614477a0b
Vanshika <vanshu0604@gmail.com>
2025-01-29 09:56:47 +0530

    made different folders

---
+++ contact/contact.js
@@ -0,0 +1,15 @@
+// document.getElementById("contactForm").addEventListener("submit", function (e) {
+//     e.preventDefault(); // Prevent default form submission
+
+//     const email = document.getElementById("email").value;
+//     const message = document.getElementById("message").value;
+
+//     // Validate email format
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     if (!emailRegex.test(email)) {
+//       alert("Please enter a valid email address!");
+//       return;
+//     }
+
+//     alert("Form is valid. Submitting now.");
+//   });
